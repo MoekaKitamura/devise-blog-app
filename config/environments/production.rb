34333d3bf6b4eb7f4ad8de8e9e2c110ecd15247a
moeka kitamura <moeka.k76@gmail.com>
2022-11-18 00:17:18 +0900

    set mailer default url options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,6 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "devise_sample_app_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'devise-blog-app.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
 
   # Ignore bad email addresses and do not raise email delivery errors.
