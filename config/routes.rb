99ecf665efe12864971dfbe4e6cefb876563bda7
moeka kitamura <moeka.k76@gmail.com>
2022-11-20 12:28:56 +0900

    add admin page with rails_admin

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   
   devise_for :users, controllers: { confirmations: 'confirmations' }
   resources :blogs
-
+  
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
